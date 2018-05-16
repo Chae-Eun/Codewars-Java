b048d5be71ddb4dfaf8c0aa7cd0a2158832b4fe3
Chae-Eun <ddangkong245@gmail.com>
2018-05-16 15:29:51 +0900

    Update Opposite

---
+++ src/Opposite.java
@@ -0,0 +1,14 @@
+/**************************MY SOLUTION******************************/
+public class Opposite{
+    public static int opposite(int number){
+        return number == 0 ? number : -number;
+    }
+}
+
+/************************BEST SOLUTION******************************/
+public class Opposite{
+    public static int opposite(int number){
+        return -number;
+    }
+}
+/*******************************************************************/
